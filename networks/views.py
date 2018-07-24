fe8a73fdab749074fe4462d73b333556385c9254
Ghaith Tarawneh <gtarawneh@gmail.com>
2018-07-24 16:30:21 +0100

    Add API decorators

--- networks/views.py
+++ networks/views.py
@@ -1,10 +1,36 @@
-from django.shortcuts import render
-
-# Create your views here.
-
 from django.http import HttpResponse
+from django.http import JsonResponse
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 
+def restful(handler):
+    """Create a Django view from a function with type json -> json."""
+
+    def restful_handler(*args, **kwargs):
+        request = kwargs['request'] if 'request' in kwargs else args[0]
+
+        if request.method == 'POST':
+            data = json.loads(request.body) if request.body else {}
+            results = handler(data)
+            return JsonResponse(results)
+        else:
+            return JsonResponse({"error": "no POST data"})
+
+    return restful_handler
+
+
+def api(func):
+    """Apply several decorators to create API-ready view function."""
+
+    decorators = [restful, csrf_exempt]
+
+    for dec in decorators:
+        func = dec(func)
+
+    return func
+
+
+@api
 def hello(request):
-    return HttpResponse("Hello networks!")
-
+    return {"result": "Hello networks!"}
